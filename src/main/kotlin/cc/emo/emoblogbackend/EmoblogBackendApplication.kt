cbe18753103256163acdf79eaa183a3a74d1cddd
Brad Chen <lrnbrad@gmail.com>
2025-10-24 10:42:46 +0800

    initialize emoblog-backend project with Gradle and Spring Boot setup

---
+++ src/main/kotlin/cc/emo/emoblogbackend/EmoblogBackendApplication.kt
@@ -0,0 +1,11 @@
+package cc.emo.emoblogbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmoblogBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<EmoblogBackendApplication>(*args)
+}
---
+++ src/test/kotlin/cc/emo/emoblogbackend/EmoblogBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package cc.emo.emoblogbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmoblogBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
