fb276ea6a8a37796fa086a5d222b50d082465e4d
Brad Chen <lrnbrad@gmail.com>
2025-10-28 15:21:26 +0800

    feat(adapter): map user entity to UserProfile dto

--- src/main/kotlin/cc/emo/emoblogbackend/adapter/UserAdapter.kt
+++ src/main/kotlin/cc/emo/emoblogbackend/adapter/UserAdapter.kt
@@ -1,2 +1,13 @@
 package cc.emo.emoblogbackend.adapter
 
+import cc.emo.emoblogbackend.data.`do`.UserDo
+import cc.emo.emoblogbackend.data.dto.UserProfileDto
+
+fun UserDo.toUserProfileDto(): UserProfileDto {
+    return UserProfileDto(
+        username = username,
+        firstName = firstName,
+        lastName = lastName,
+        birth = birthday,
+    )
+}
