6f2bd65da3320204cc0f1b98262fad42a801320b
Brad Chen <lrnbrad@gmail.com>
2025-11-07 21:04:33 +0800

    feat(adapter): map User/Post entities to new DTO responses and requests

--- src/main/kotlin/cc/emo/emoblogbackend/adapter/PostAdapter.kt
+++ src/main/kotlin/cc/emo/emoblogbackend/adapter/PostAdapter.kt
@@ -1,21 +1,45 @@
 package cc.emo.emoblogbackend.adapter
 
+import cc.emo.emoblogbackend.data.`do`.PostCommentDo
+import cc.emo.emoblogbackend.data.`do`.PostDo
 import cc.emo.emoblogbackend.data.`do`.UserDo
-import cc.emo.emoblogbackend.data.dto.UserProfileDto
-import cc.emo.emoblogbackend.data.dto.UserProfileUpdateRequestDto
+import cc.emo.emoblogbackend.data.dto.PostCommentResponse
+import cc.emo.emoblogbackend.data.dto.PostResponse
+import cc.emo.emoblogbackend.data.dto.UserProfile
+import cc.emo.emoblogbackend.data.dto.UserProfileUpdateRequest
 
 // TODO: find same birth can make use of it
-fun UserDo.toUserProfileResponse() = UserProfileDto(
+fun UserDo.toUserProfileResponse() = UserProfile(
 //    id = id,
     username = username,
     firstName = firstName,
     lastName = lastName,
-    birth = birth,
+    birthday = birthday,
 )
 
-fun UserDo.toUserProfileUpdate() = UserProfileUpdateRequestDto(
+fun UserDo.toUserProfileUpdate() = UserProfileUpdateRequest(
     firstName = firstName,
     lastName = lastName,
-    birth = birth,
+    birthday = birthday,
     newPasswordHash = passwordHash
+)
+
+fun PostDo.toPostResponse(commentCount: Long = comments.size.toLong()) = PostResponse(
+    id = id,
+    authorId = author.id,
+    content = content,
+    likeCount = likeCount,
+    createdAt = createdAt ?: error("Post entity has no createdAt; ensure it is persisted"),
+    updatedAt = updatedAt,
+    commentCount = commentCount
+)
+
+fun PostCommentDo.toPostCommentResponse(fallbackPostId: Long? = null) = PostCommentResponse(
+    id = id,
+    postId = post?.id ?: fallbackPostId ?: error("Detached comment missing post reference"),
+    authorId = author.id,
+    content = content,
+    likeCount = likeCount,
+    createdAt = createdAt ?: error("Comment entity has no createdAt; ensure it is persisted"),
+    updatedAt = updatedAt
 )
