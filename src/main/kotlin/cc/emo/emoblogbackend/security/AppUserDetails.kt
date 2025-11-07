71cc409ff62b316157713ca061c9d84cb1e22322
Brad Chen <lrnbrad@gmail.com>
2025-11-07 20:48:26 +0800

    feat(security): allow custom authorities and add factory helper

--- src/main/kotlin/cc/emo/emoblogbackend/security/AppUserDetails.kt
+++ src/main/kotlin/cc/emo/emoblogbackend/security/AppUserDetails.kt
@@ -1,17 +1,28 @@
 package cc.emo.emoblogbackend.security
 
 import cc.emo.emoblogbackend.data.`do`.UserDo
+import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
-class AppUserDetails(val user: UserDo) : UserDetails {
+class AppUserDetails(
+    val user: UserDo,
+    private val authorities: Collection<GrantedAuthority> = listOf(SimpleGrantedAuthority("ROLE_USER"))
+) : UserDetails {
     override fun getUsername() = user.username
     override fun getPassword() = user.passwordHash
-    override fun getAuthorities() = listOf(SimpleGrantedAuthority("ROLE_USER"))
+    override fun getAuthorities(): Collection<GrantedAuthority> = authorities
     override fun isAccountNonExpired() = true
     override fun isAccountNonLocked() = true
     override fun isCredentialsNonExpired() = true
     override fun isEnabled() = true
 
     fun id(): Long = user.id
+
+    companion object {
+        fun from(user: UserDo, roles: Collection<String> = listOf("ROLE_USER")): AppUserDetails {
+            val granted = roles.map(::SimpleGrantedAuthority)
+            return AppUserDetails(user, granted)
+        }
+    }
 }
