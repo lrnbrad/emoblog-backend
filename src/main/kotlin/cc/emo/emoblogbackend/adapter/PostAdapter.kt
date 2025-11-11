a94472f00adc3de63d7b8bc920606eefe3eea5b7
Brad Chen <lrnbrad@gmail.com>
2025-11-11 16:42:16 +0800

    chore(PostDto): remove duplicated converter

--- src/main/kotlin/cc/emo/emoblogbackend/adapter/PostAdapter.kt
+++ src/main/kotlin/cc/emo/emoblogbackend/adapter/PostAdapter.kt
@@ -2,27 +2,8 @@ package cc.emo.emoblogbackend.adapter
 
 import cc.emo.emoblogbackend.data.`do`.PostCommentDo
 import cc.emo.emoblogbackend.data.`do`.PostDo
-import cc.emo.emoblogbackend.data.`do`.UserDo
 import cc.emo.emoblogbackend.data.dto.PostCommentResponse
 import cc.emo.emoblogbackend.data.dto.PostResponse
-import cc.emo.emoblogbackend.data.dto.UserProfile
-import cc.emo.emoblogbackend.data.dto.UserProfileUpdateRequest
-
-// TODO: find same birth can make use of it
-fun UserDo.toUserProfileResponse() = UserProfile(
-//    id = id,
-    username = username,
-    firstName = firstName,
-    lastName = lastName,
-    birthday = birthday,
-)
-
-fun UserDo.toUserProfileUpdate() = UserProfileUpdateRequest(
-    firstName = firstName,
-    lastName = lastName,
-    birthday = birthday,
-    newPasswordHash = passwordHash
-)
 
 fun PostDo.toPostResponse(commentCount: Long = comments.size.toLong()) = PostResponse(
     id = id,
